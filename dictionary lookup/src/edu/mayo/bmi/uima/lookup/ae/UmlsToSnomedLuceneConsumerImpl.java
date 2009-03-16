40458bda91809f3b1744f322b54e12ec37d3e6b7
James Joseph Masanz <james-masanz@apache.org>
2009-03-16 17:38:33 +0000

    Initial import.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/trunk/cTAKES@1385412 13f79535-47bb-0310-9956-ffa450edef68

---
+++ dictionary lookup/src/edu/mayo/bmi/uima/lookup/ae/UmlsToSnomedLuceneConsumerImpl.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright: (c) 2009   Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+package edu.mayo.bmi.uima.lookup.ae;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.uima.analysis_engine.annotator.AnnotatorContext;
+
+import edu.mayo.bmi.dictionary.DictionaryException;
+import edu.mayo.bmi.dictionary.MetaDataHit;
+import edu.mayo.bmi.dictionary.lucene.LuceneDictionaryImpl;
+import edu.mayo.bmi.uima.core.resource.FileLocator;
+
+/**
+ * Implementation that takes UMLS dictionary lookup hits and stores as NamedEntity 
+ * objects only the ones that have a SNOMED synonym, by looking in a lucene index 
+ * for SNOMED codes that map to the identified CUI.
+ * 
+ * @author Mayo Clinic
+ */
+public class UmlsToSnomedLuceneConsumerImpl extends UmlsToSnomedConsumerImpl implements
+		LookupConsumer
+{
+
+	// LOG4J logger based on class name
+	private Logger logger = Logger.getLogger(getClass().getName());
+
+
+	private final String SNOMED_MAPPING_PRP_KEY = "snomedCodeMappingField";
+	private final String CUI_MAPPING_PRP_KEY = "cuiMappingField";
+
+	public UmlsToSnomedLuceneConsumerImpl(AnnotatorContext aCtx, Properties properties)
+			throws Exception
+	{
+		super(aCtx,properties);
+	}
+
+
+	/**
+	 * Find all Snomed codes that map to the given UMLS code (CUI),
+	 * by looking in a lucene index
+	 * 
+	 * @param umlsCode a UMLS CUI
+	 * @return Set of Snomed codes that map to the given UMLS code (CUI).
+	 * @see getSnomedCodes in <code>UmlsToSnomedConsumerImpl</code> for example of using a database
+	 */
+	protected Set getSnomedCodes(String umlsCode) throws DictionaryException
+	{
+		Set codeSet = new HashSet();
+		
+		IndexReader indexReader;
+		String indexPath = null;
+		String indexDirAbsPath = null;
+		try {
+			// For the sample dictionary, we use the following lucene index.
+			indexPath = "lookup/snomed-like_codes_sample";
+			File indexDir = FileLocator.locateFile(indexPath); // TODO parameterize this
+			indexDirAbsPath = indexDir.getAbsolutePath();
+			
+			indexReader = IndexReader.open(indexDirAbsPath); 
+
+			IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+			String lookupFieldName = props.getProperty(CUI_MAPPING_PRP_KEY);
+			String valueFieldName = props.getProperty(SNOMED_MAPPING_PRP_KEY);
+			
+			// We will lookup entries based on lookupFieldName
+			LuceneDictionaryImpl snomedLikeCodesIndex = new LuceneDictionaryImpl(indexSearcher, lookupFieldName);
+			
+	        logger.info("Loaded Lucene index with "+ indexReader.numDocs() +" entries.");
+	
+	        // Get the entries with field lookupFieldName having value umlsCode
+	        Collection<MetaDataHit> mdhCollection = snomedLikeCodesIndex.getEntries(umlsCode);
+	        
+	        for (MetaDataHit mdh: mdhCollection) {
+	        	codeSet.add(mdh.getMetaFieldValue(valueFieldName));
+	        }
+		} catch (IOException ioe) {
+			
+	        logger.info("Lucene index: " + indexDirAbsPath);
+			throw new DictionaryException(ioe);
+		}
+		
+		return codeSet;
+
+	}
+
+}
