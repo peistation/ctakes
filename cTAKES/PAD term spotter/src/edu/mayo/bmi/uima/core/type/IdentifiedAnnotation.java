29e449cd927785a5bdc4b6a6cad9b0ae7fac05af
Sean Patrick Murphy <spmurphy50@apache.org>
2010-07-27 20:25:31 +0000

    Share project "PAD term spotter" into "https://ohnlp.svn.sourceforge.net/svnroot/ohnlp"
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/trunk@1385506 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cTAKES/PAD term spotter/src/edu/mayo/bmi/uima/core/type/IdentifiedAnnotation.java
@@ -0,0 +1,80 @@
+
+
+/* First created by JCasGen Tue Jul 20 20:16:45 CDT 2010 */
+package edu.mayo.bmi.uima.core.type;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Tue Jul 20 20:16:45 CDT 2010
+ * XML source: C:/general_workspace/PAD term spotter/desc/type_system/PADSiteAndTerm.xml
+ * @generated */
+public class IdentifiedAnnotation extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  public final static int typeIndexID = JCasRegistry.register(IdentifiedAnnotation.class);
+  /** @generated
+   * @ordered 
+   */
+  public final static int type = typeIndexID;
+  /** @generated  */
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected IdentifiedAnnotation() {}
+    
+  /** Internal - constructor used by generator 
+   * @generated */
+  public IdentifiedAnnotation(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated */
+  public IdentifiedAnnotation(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated */  
+  public IdentifiedAnnotation(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** <!-- begin-user-doc -->
+    * Write your own initialization here
+    * <!-- end-user-doc -->
+  @generated modifiable */
+  private void readObject() {}
+     
+ 
+    
+  //*--------------*
+  //* Feature: uid
+
+  /** getter for uid - gets Unique identifier of this annotation.
+   * @generated */
+  public int getUid() {
+    if (IdentifiedAnnotation_Type.featOkTst && ((IdentifiedAnnotation_Type)jcasType).casFeat_uid == null)
+      jcasType.jcas.throwFeatMissing("uid", "edu.mayo.bmi.uima.core.type.IdentifiedAnnotation");
+    return jcasType.ll_cas.ll_getIntValue(addr, ((IdentifiedAnnotation_Type)jcasType).casFeatCode_uid);}
+    
+  /** setter for uid - sets Unique identifier of this annotation. 
+   * @generated */
+  public void setUid(int v) {
+    if (IdentifiedAnnotation_Type.featOkTst && ((IdentifiedAnnotation_Type)jcasType).casFeat_uid == null)
+      jcasType.jcas.throwFeatMissing("uid", "edu.mayo.bmi.uima.core.type.IdentifiedAnnotation");
+    jcasType.ll_cas.ll_setIntValue(addr, ((IdentifiedAnnotation_Type)jcasType).casFeatCode_uid, v);}    
+  }
+
+    
