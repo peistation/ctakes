b25a31cdcc937341f7360e265bded5a82f4f6dfc
Pei Chen <chenpei@apache.org>
2011-11-15 21:03:28 +0000

    Fix for https://sourceforge.net/tracker/?func=detail&aid=3438483&group_id=255545&atid=1126903
    core/JdbcConnectionResourceImpl.java is commented out so by default, jdbc connections will not work out of the box.
    This was due to an incompatible licenese with the MIR autoreconnect classes.
    
    -Uncomment out JdbcConnectionResourceImpl
    -Include sqlWrapper.jar
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/trunk/cTAKES@1385687 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/edu/mayo/bmi/uima/core/resource/JdbcConnectionResourceImpl.java
+++ core/src/edu/mayo/bmi/uima/core/resource/JdbcConnectionResourceImpl.java
@@ -21,123 +21,123 @@
  * See the License for the specific language governing permissions and 
  * limitations under the License. 
  */
-//package edu.mayo.bmi.uima.core.resource;
-//
-//import java.lang.reflect.Field;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//
-//import org.apache.log4j.Logger;
-//
-//import org.apache.uima.resource.DataResource;
-//import org.apache.uima.resource.ResourceInitializationException;
-//import org.apache.uima.resource.SharedResourceObject;
-//import org.apache.uima.resource.metadata.ConfigurationParameterSettings;
-//
-//import edu.mayo.mir.utility.sqlReconnect.WrappedConnection;
-//
-///**
-// * Implementation of JdbcConnectionResource. Uses the WrappedConnection
-// * from the MIR Utility project mostly for it's auto-reconnect capability.
-// * 
-// * @author Mayo Clinic
-// */
-//public class JdbcConnectionResourceImpl implements JdbcConnectionResource,
-//		SharedResourceObject
-//{
-//    private Logger iv_logger = Logger.getLogger(getClass().getName());
-//
-//    /**
-//	 * JDBC driver ClassName.
-//	 */
-//	public static final String PARAM_DRIVER_CLASS = "DriverClassName";
-//
-//	/**
-//	 * JDBC URL that specifies db network location and db name.
-//	 */
-//	public static final String PARAM_URL = "URL";
-//
-//	/**
-//	 * Username for db authentication.
-//	 */
-//	public static final String PARAM_USERNAME = "Username";
-//
-//	/**
-//	 * Password for db authentication.
-//	 */
-//	public static final String PARAM_PASSWORD = "Password";
-//
-//	/**
-//	 * Flag that determines whether to keep JDBC connection open no matter what.
-//	 */
-//	public static final String PARAM_KEEP_ALIVE = "KeepConnectionAlive";
-//
-//    /**
-//     * Transaction isolation level.  Value should be a static fieldname from
-//     * java.sql.Connection such as TRANSACTION_READ_UNCOMMITTED.  This parameter
-//     * is optional. 
-//     */
-//    public static final String PARAM_ISOLATION = "TransactionIsolation";    
-//    
-//	private Connection iv_conn;
-//
-//	public void load(DataResource dr) throws ResourceInitializationException
-//	{
-//		ConfigurationParameterSettings cps = dr.getMetaData()
-//				.getConfigurationParameterSettings();
-//
-//		String driverClassName = (String) cps.getParameterValue(PARAM_DRIVER_CLASS);
-//
-//		String urlStr = (String) cps.getParameterValue(PARAM_URL);
-//
-//		String username = (String) cps.getParameterValue(PARAM_USERNAME);
-//
-//		String password = (String) cps.getParameterValue(PARAM_PASSWORD);
-//
-//		Boolean keepAlive = new Boolean((String) cps.getParameterValue(PARAM_KEEP_ALIVE));
-//        
-//        String isolationStr = (String) cps.getParameterValue(PARAM_ISOLATION);
-//        
-//		try
-//		{            
-//			if (keepAlive.booleanValue())
-//			{
-//                iv_logger.info("Instantiating wrapped connection.");
-//				iv_conn = new WrappedConnection(username,
-//						password,
-//						driverClassName,
-//						urlStr);
-//			}
-//			else
-//			{
-//				Class.forName(driverClassName);
-//				iv_conn = DriverManager.getConnection(
-//						urlStr,
-//						username,
-//						password);
-//			}
-//
-//			iv_logger.info("Connection established to: " + urlStr);
-//            
-//            if (isolationStr != null)
-//            {
-//                // use java reflection to obtain the corresponding level integer
-//                Class connClass = Class.forName("java.sql.Connection");
-//                Field f = connClass.getField(isolationStr);
-//                int level = f.getInt(null);
-//                iv_logger.info("Connection transaction isolation level set: " +
-//                        isolationStr + "(" + level +")");
-//                iv_conn.setTransactionIsolation(level);
-//            }            
-//		}
-//		catch (Exception e)
-//		{
-//			throw new ResourceInitializationException(e);
-//		}
-//	}
-//
-//	public Connection getConnection()
-//	{
-//		return iv_conn;
-//	}
-//}
+package edu.mayo.bmi.uima.core.resource;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.apache.log4j.Logger;
+
+import org.apache.uima.resource.DataResource;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.SharedResourceObject;
+import org.apache.uima.resource.metadata.ConfigurationParameterSettings;
+
+import sqlWrapper.WrappedConnection;
+
+/**
+ * Implementation of JdbcConnectionResource. Uses the WrappedConnection
+ * from the MIR Utility project mostly for it's auto-reconnect capability.
+ * 
+ * @author Mayo Clinic
+ */
+public class JdbcConnectionResourceImpl implements JdbcConnectionResource,
+		SharedResourceObject
+{
+    private Logger iv_logger = Logger.getLogger(getClass().getName());
+
+    /**
+	 * JDBC driver ClassName.
+	 */
+	public static final String PARAM_DRIVER_CLASS = "DriverClassName";
+
+	/**
+	 * JDBC URL that specifies db network location and db name.
+	 */
+	public static final String PARAM_URL = "URL";
+
+	/**
+	 * Username for db authentication.
+	 */
+	public static final String PARAM_USERNAME = "Username";
+
+	/**
+	 * Password for db authentication.
+	 */
+	public static final String PARAM_PASSWORD = "Password";
+
+	/**
+	 * Flag that determines whether to keep JDBC connection open no matter what.
+	 */
+	public static final String PARAM_KEEP_ALIVE = "KeepConnectionAlive";
+
+    /**
+     * Transaction isolation level.  Value should be a static fieldname from
+     * java.sql.Connection such as TRANSACTION_READ_UNCOMMITTED.  This parameter
+     * is optional. 
+     */
+    public static final String PARAM_ISOLATION = "TransactionIsolation";    
+    
+	private Connection iv_conn;
+
+	public void load(DataResource dr) throws ResourceInitializationException
+	{
+		ConfigurationParameterSettings cps = dr.getMetaData()
+				.getConfigurationParameterSettings();
+
+		String driverClassName = (String) cps.getParameterValue(PARAM_DRIVER_CLASS);
+
+		String urlStr = (String) cps.getParameterValue(PARAM_URL);
+
+		String username = (String) cps.getParameterValue(PARAM_USERNAME);
+
+		String password = (String) cps.getParameterValue(PARAM_PASSWORD);
+
+		Boolean keepAlive = new Boolean((String) cps.getParameterValue(PARAM_KEEP_ALIVE));
+        
+        String isolationStr = (String) cps.getParameterValue(PARAM_ISOLATION);
+        
+		try
+		{            
+			if (keepAlive.booleanValue())
+			{
+                iv_logger.info("Instantiating wrapped connection.");
+				iv_conn = new WrappedConnection(username,
+						password,
+						driverClassName,
+						urlStr);
+			}
+			else
+			{
+				Class.forName(driverClassName);
+				iv_conn = DriverManager.getConnection(
+						urlStr,
+						username,
+						password);
+			}
+
+			iv_logger.info("Connection established to: " + urlStr);
+            
+            if (isolationStr != null)
+            {
+                // use java reflection to obtain the corresponding level integer
+                Class connClass = Class.forName("java.sql.Connection");
+                Field f = connClass.getField(isolationStr);
+                int level = f.getInt(null);
+                iv_logger.info("Connection transaction isolation level set: " +
+                        isolationStr + "(" + level +")");
+                iv_conn.setTransactionIsolation(level);
+            }            
+		}
+		catch (Exception e)
+		{
+			throw new ResourceInitializationException(e);
+		}
+	}
+
+	public Connection getConnection()
+	{
+		return iv_conn;
+	}
+}
