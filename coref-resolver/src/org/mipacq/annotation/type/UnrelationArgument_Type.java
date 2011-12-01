f84b024b2c561f94a3c57e718689a50b17e6ea37
Pei Chen <chenpei@apache.org>
2011-12-01 19:33:46 +0000

    Initial check in of coref-resolver https://sourceforge.net/tracker/?func=detail&aid=3447321&group_id=255545&atid=1126903
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/trunk/cTAKES@1385701 13f79535-47bb-0310-9956-ffa450edef68

---
+++ coref-resolver/src/org/mipacq/annotation/type/UnrelationArgument_Type.java
@@ -0,0 +1,54 @@
+
+/* First created by JCasGen Wed Aug 31 13:40:25 EDT 2011 */
+package org.mipacq.annotation.type;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+
+/** A relation argument for a negative example.  Used mainly for debugging.
+ * Updated by JCasGen Tue Nov 22 14:28:43 EST 2011
+ * @generated */
+public class UnrelationArgument_Type extends RelationArgument_Type {
+  /** @generated */
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+  /** @generated */
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+  			 if (UnrelationArgument_Type.this.useExistingInstance) {
+  			   // Return eq fs instance if already created
+  		     FeatureStructure fs = UnrelationArgument_Type.this.jcas.getJfsFromCaddr(addr);
+  		     if (null == fs) {
+  		       fs = new UnrelationArgument(addr, UnrelationArgument_Type.this);
+  			   UnrelationArgument_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  			   return fs;
+  		     }
+  		     return fs;
+        } else return new UnrelationArgument(addr, UnrelationArgument_Type.this);
+  	  }
+    };
+  /** @generated */
+  public final static int typeIndexID = UnrelationArgument.typeIndexID;
+  /** @generated 
+     @modifiable */
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("org.mipacq.annotation.type.UnrelationArgument");
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	* @generated */
+  public UnrelationArgument_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+  }
+}
+
+
+
+    
