526375b1ef4a45782cd5f14d78a032befc37c8f1
Pei Chen <chenpei@apache.org>
2012-01-12 20:24:50 +0000

    Creating Skeleton project for relation-extractor https://sourceforge.net/tracker/?group_id=255545&atid=1126903
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1385753 13f79535-47bb-0310-9956-ffa450edef68

---
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/ae/RelationExtractor.java
@@ -0,0 +1,40 @@
+/*    Copyright 2011 Children's Hospital Boston
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+    
+@author
+ * 
+ */
+package org.chboston.cnlp.ctakes.relationextractor.ae;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+
+public class RelationExtractor extends JCasAnnotator_ImplBase {
+
+	public void initialize(UimaContext context)
+			throws ResourceInitializationException {
+		super.initialize(context);
+		// TODO: Your init stuff here
+
+	}
+
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		// TODO: Your Process Here
+
+	}
+
+}
