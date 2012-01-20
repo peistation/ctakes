831c5069c2a93465166d3c8bc527eb8fccac648b
Pei Chen <chenpei@apache.org>
2012-01-20 16:00:25 +0000

    Initial check in of the Common Type System implementation. The main noticeable changes include: Refactoring: NamedEntity -> IdentifiedAnnotation Uid -> Id Status -> Uncertainty Uncertainty -> Polarity
    New packages for existing types. Ex.:
    edu.mayo.bmi.uima.core.type.syntax.BaseToken
    edu.mayo.bmi.uima.core.type.syntax.WordToken
    edu.mayo.bmi.uima.core.type.textspan.Segment
    edu.mayo.bmi.uima.core.type.textsem.IdentifiedAnnotation
    edu.mayo.bmi.uima.core.type.refsem.OntologyConcept
    
    Deprecated UIMA code updated:
    JTextAnnotator_ImplBase > JCasAnnotator_ImplBase
    AnnotatorContext -> UimaContext
    AnnotatorInitializationException, AnnotatorConfigurationException -> ResourceInitializationException
    AnnotatorProcessException  -> AnalysisEngineProcessException
    
    files for pipeline root/build.xml scripts updated to reflect new projects.
    https://sourceforge.net/tracker/?func=detail&aid=3476666&group_id=255545&atid=1126903
    https://sourceforge.net/tracker/?func=detail&aid=3470399&group_id=255545&atid=1126903
    https://sourceforge.net/tracker/?func=detail&aid=3470398&group_id=255545&atid=1126903
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1385776 13f79535-47bb-0310-9956-ffa450edef68

--- Constituency Parser/src/org/chboston/cnlp/ctakes/parser/MaxentParserWrapper.java
+++ Constituency Parser/src/org/chboston/cnlp/ctakes/parser/MaxentParserWrapper.java
@@ -35,12 +35,12 @@ import org.chboston.cnlp.ctakes.parser.uima.type.TerminalTreebankNode;
 import org.chboston.cnlp.ctakes.parser.uima.type.TopTreebankNode;
 import org.chboston.cnlp.ctakes.parser.uima.type.TreebankNode;
 
-import edu.mayo.bmi.uima.core.type.BaseToken;
-import edu.mayo.bmi.uima.core.type.NewlineToken;
-import edu.mayo.bmi.uima.core.type.PunctuationToken;
-import edu.mayo.bmi.uima.core.type.Sentence;
-import edu.mayo.bmi.uima.core.type.SymbolToken;
-import edu.mayo.bmi.uima.core.type.WordToken;
+import edu.mayo.bmi.uima.core.type.syntax.BaseToken;
+import edu.mayo.bmi.uima.core.type.syntax.NewlineToken;
+import edu.mayo.bmi.uima.core.type.syntax.PunctuationToken;
+import edu.mayo.bmi.uima.core.type.textspan.Sentence;
+import edu.mayo.bmi.uima.core.type.syntax.SymbolToken;
+import edu.mayo.bmi.uima.core.type.syntax.WordToken;
 
 public class MaxentParserWrapper implements ParserWrapper {
 
