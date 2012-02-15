cd76463e7880eaccffcdc16040f496bee416a90c
weitechen <weitechen@unknown>
2012-02-15 18:54:52 +0000

    Add chunker extractor
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1385912 13f79535-47bb-0310-9956-ffa450edef68

---
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/ae/ChunkerExtractor.java
@@ -0,0 +1,60 @@
+package org.chboston.cnlp.ctakes.relationextractor.ae;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+import org.cleartk.classifier.Feature;
+import org.cleartk.classifier.feature.extractor.ContextExtractor;
+import org.cleartk.classifier.feature.extractor.ContextExtractor.Bag;
+import org.cleartk.classifier.feature.extractor.ContextExtractor.Covered;
+import org.cleartk.classifier.feature.extractor.simple.NamingExtractor;
+import org.cleartk.classifier.feature.extractor.simple.SimpleFeatureExtractor;
+import org.cleartk.classifier.feature.extractor.simple.TypePathExtractor;
+
+import edu.mayo.bmi.uima.core.type.syntax.BaseToken;
+import edu.mayo.bmi.uima.core.type.syntax.Chunk;
+import edu.mayo.bmi.uima.core.type.textsem.IdentifiedAnnotation;
+
+
+public class ChunkerExtractor implements RelationFeaturesExtractor {
+
+	  /**
+	   * All chunk tags of the mention as a bag
+	   */
+	
+	  private SimpleFeatureExtractor chunker = new TypePathExtractor(Chunk.class, "chunkType");
+	  
+	  private SimpleFeatureExtractor tokenChunk = new ContextExtractor<Chunk>(
+		  Chunk.class,
+	      chunker,
+	      new Bag(new Covered()));
+
+	  /**
+	   * All extractors for mention 1, with features named to distinguish them from mention 2
+	   */
+	  private SimpleFeatureExtractor mention1FeaturesExtractor = new NamingExtractor(
+	      "mention1",
+	      tokenChunk);
+
+	  /**
+	   * All extractors for mention 2, with features named to distinguish them from mention 1
+	   */
+	  private SimpleFeatureExtractor mention2FeaturesExtractor = new NamingExtractor(
+	      "mention2",
+	      tokenChunk);
+
+	@Override
+	public List<Feature> extract(JCas jCas, IdentifiedAnnotation arg1,
+			IdentifiedAnnotation arg2) throws AnalysisEngineProcessException {
+		
+		List<Feature> features = new ArrayList<Feature>();
+		
+		features.addAll(this.mention1FeaturesExtractor.extract(jCas, arg1));
+		features.addAll(this.mention2FeaturesExtractor.extract(jCas, arg2));
+
+		return features;
+	}
+
+}
