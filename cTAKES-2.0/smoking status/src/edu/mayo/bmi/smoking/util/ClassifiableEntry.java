ab968f138ac6efe8f6a045a3d26d1ffdf314249f
James Joseph Masanz <james-masanz@apache.org>
2012-03-27 16:25:24 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/tags@1386025 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cTAKES-2.0/smoking status/src/edu/mayo/bmi/smoking/util/ClassifiableEntry.java
@@ -0,0 +1,10 @@
+package edu.mayo.bmi.smoking.util;
+
+public class ClassifiableEntry
+{
+    public String iv_recordID;
+    public int iv_begin;
+    public int iv_end;
+    public String iv_text;
+    public String iv_classification = null; // optional
+}
