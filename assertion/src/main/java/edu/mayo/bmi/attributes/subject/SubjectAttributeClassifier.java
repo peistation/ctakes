66bdeaaa670678bb41b2aab906a6eb75022331f6
stephenwu <stephenwu@unknown>
2012-04-11 22:13:08 +0000

    added analysis engines for the subject and generic classifiers.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1386112 13f79535-47bb-0310-9956-ffa450edef68

--- assertion/src/main/java/edu/mayo/bmi/attributes/subject/SubjectAttributeClassifier.java
+++ assertion/src/main/java/edu/mayo/bmi/attributes/subject/SubjectAttributeClassifier.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright: (c) 2012   Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
 package edu.mayo.bmi.attributes.subject;
 
 import java.io.IOException;
@@ -122,20 +145,20 @@ public class SubjectAttributeClassifier {
 		List<ConllDependencyNode> depnodes = JCasUtil.selectCovered(jCas, ConllDependencyNode.class, mention);
 		if (!depnodes.isEmpty()) {
 			ConllDependencyNode depnode = DependencyUtility.getNominalHeadNode(depnodes);
-//			for (ConllDependencyNode dn : DependencyUtility.getPathToTop(jCas, depnode)) {
-//				if ( isDonorTerm(dn) ) {
-//					vfeat.put(DONOR_DEPPATH, true);
-//				}
-//				if ( isFamilyTerm(dn) ) {
-//					vfeat.put(FAMILY_DEPPATH, true);
-//				}
-//				if ( isOtherTerm(dn) ) {
-//					vfeat.put(OTHER_DEPPATH, true);
-//				}
-//				
-//				// check if there is SRL stuff on the dependency path
-//				
-//			}
+			for (ConllDependencyNode dn : DependencyUtility.getPathToTop(jCas, depnode)) {
+				if ( isDonorTerm(dn) ) {
+					vfeat.put(DONOR_DEPPATH, true);
+				}
+				if ( isFamilyTerm(dn) ) {
+					vfeat.put(FAMILY_DEPPATH, true);
+				}
+				if ( isOtherTerm(dn) ) {
+					vfeat.put(OTHER_DEPPATH, true);
+				}
+				
+				// check if there is SRL stuff on the dependency path
+				
+			}
 		}
 		
 		// Logic to identify cases, may be replaced by learned classification
---
+++ assertion/src/main/java/edu/mayo/bmi/uima/attributes/GenericAttributeAnalysisEngine.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright: (c) 2012   Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+package edu.mayo.bmi.uima.attributes;
+
+import org.apache.log4j.Logger;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.uimafit.component.JCasAnnotator_ImplBase;
+import org.uimafit.util.JCasUtil;
+
+import edu.mayo.bmi.attributes.generic.GenericAttributeClassifier;
+import edu.mayo.bmi.uima.core.type.textsem.IdentifiedAnnotation;
+
+/**
+ * <br>
+* This class provides a basic generic attribute extraction technique based on dependency parses.
+ * <p>
+ * 
+ * 
+ */
+
+public class GenericAttributeAnalysisEngine extends JCasAnnotator_ImplBase {
+
+	public Logger logger = Logger.getLogger(getClass().getName());
+	
+	@Override
+	public void initialize(UimaContext context) throws ResourceInitializationException {
+		super.initialize(context);
+	}
+
+	@Override
+	public void process(JCas jCas) throws AnalysisEngineProcessException {
+		for (IdentifiedAnnotation mention : JCasUtil.select(jCas, IdentifiedAnnotation.class)) {
+
+				Boolean oldgeneric = mention.getGeneric();
+				mention.setGeneric( GenericAttributeClassifier.getGeneric(jCas, mention) );
+//				System.err.println("Word:"+mention.getCoveredText()+", overwrote generic "+oldgeneric+" with "+mention.getGeneric());
+		}
+		
+		
+	}
+}
