7ab863038f6a2fe1c974aef8cb773d4f94993628
dimdym <dimdym@unknown>
2012-04-17 20:41:50 +0000

    Combined two readers (for degree_of and location_of relations) into one.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1386154 13f79535-47bb-0310-9956-ffa450edef68

--- relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/cr/GoldEntityAndRelationReader.java
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/cr/GoldEntityAndRelationReader.java
@@ -24,6 +24,7 @@ import edu.mayo.bmi.uima.core.type.constants.CONST;
 import edu.mayo.bmi.uima.core.type.relation.BinaryTextRelation;
 import edu.mayo.bmi.uima.core.type.relation.RelationArgument;
 import edu.mayo.bmi.uima.core.type.textsem.EntityMention;
+import edu.mayo.bmi.uima.core.type.textsem.Modifier;
 import edu.mayo.bmi.uima.core.util.DocumentIDAnnotationUtil;
 
 /**
@@ -102,11 +103,165 @@ public class GoldEntityAndRelationReader extends JCasAnnotator_ImplBase {
 		// get the relations and their arguments
 		ArrayList<RelationInfo> relationInfos = XMLReader.getRelations(document);
 
-		// relations between unique pairs of entities in gold standard
-		HashSet<RelationInfo> uniqueRelations = new HashSet<RelationInfo>();
 		// map unique spans of text to entity mention objects
+		// we need this ensure that every entity mention is added only once
 		HashMap<Span, EntityMention> spanToEntity = new HashMap<Span, EntityMention>();
 
+		addDegreeOfRelations(jCas, entityMentions, entityTypes, relationInfos, spanToEntity);
+		addTheRestOfRelations(jCas, entityMentions, entityTypes, relationInfos, spanToEntity);
+		addTheRestOfEntities(jCas, entityMentions, entityTypes, spanToEntity);
+	}
+	
+	/**
+	 * Add to the CAS instances of degree_of relations of the form:
+	 * [Argument] degree_of [Related_to] (e.g. "chronic pain")
+	 * Create a Modifier for [Argument] and an EntityMention for [Related_to].
+	 */
+	private void addDegreeOfRelations(
+			JCas jCas, 
+			HashMap<String, ArrayList<Span>> entityMentions,
+			HashMap<String, String> entityTypes,
+			ArrayList<RelationInfo> relationInfos,
+			HashMap<Span, EntityMention> spanToEntity) {
+		
+		// relations between unique pairs of entities in gold standard
+		HashSet<RelationInfo> uniqueRelations = new HashSet<RelationInfo>();
+		// map unique spans of text to modifier objects
+		HashMap<Span, Modifier> spanToModifier = new HashMap<Span, Modifier>();
+		
+		// add relations and relation arguments to the cas
+		for(RelationInfo relationInfo : relationInfos) {
+
+			// this reader only handles instances of degree_of
+			if(! relationInfo.category.equals("degree_of")) {
+				continue; 
+			}
+
+			// restrict the types of Related_to arguments to the ones that ctakes can extract
+			if(relationInfo.role1.equals(Constants.mipacqRelatedToName) &&  
+					(Mapper.getEntityTypeId(entityTypes.get(relationInfo.id1)) == CONST.NE_TYPE_ID_UNKNOWN)) {
+				continue;
+			}
+			if(relationInfo.role2.equals(Constants.mipacqRelatedToName) &&
+					Mapper.getEntityTypeId(entityTypes.get(relationInfo.id2)) == CONST.NE_TYPE_ID_UNKNOWN) {
+				continue;
+			}
+
+			// only a single relation is allowed between same pair of gold entities
+			if(uniqueRelations.contains(relationInfo)) {
+				continue;
+			}
+			uniqueRelations.add(relationInfo);
+
+			Span argumentSpan;
+			int argumentType;
+			// need to find out which of the two arguments is the first semantic argument (i.e. "Argument")
+			if(relationInfo.role1.equals(Constants.mipacqArgumentName)) {
+				Span first = entityMentions.get(relationInfo.id1).get(0);
+				Span last = entityMentions.get(relationInfo.id1).get(entityMentions.get(relationInfo.id1).size() - 1);
+				argumentSpan = new Span(first.start, last.end);
+				argumentType = Mapper.getEntityTypeId(entityTypes.get(relationInfo.id1));
+			} 
+			else if(relationInfo.role2.equals(Constants.mipacqArgumentName)) {
+				Span first = entityMentions.get(relationInfo.id2).get(0);
+				Span last = entityMentions.get(relationInfo.id2).get(entityMentions.get(relationInfo.id2).size() - 1);
+				argumentSpan = new Span(first.start, last.end);
+				argumentType = Mapper.getEntityTypeId(entityTypes.get(relationInfo.id2));
+			}
+			else {
+				continue; // invalid value in role1
+			}
+			
+			Span relatedToSpan;
+			int relatedToType;
+			// need to find out which of the two arguments is the second semantic argument (i.e. "Related_to")
+			if(relationInfo.role1.equals(Constants.mipacqRelatedToName)) {
+				Span first = entityMentions.get(relationInfo.id1).get(0);
+				Span last = entityMentions.get(relationInfo.id1).get(entityMentions.get(relationInfo.id1).size() - 1);
+				relatedToSpan = new Span(first.start, last.end);
+				relatedToType = Mapper.getEntityTypeId(entityTypes.get(relationInfo.id1));
+			} 
+			else if(relationInfo.role2.equals(Constants.mipacqRelatedToName)) {
+				Span first = entityMentions.get(relationInfo.id2).get(0);
+				Span last = entityMentions.get(relationInfo.id2).get(entityMentions.get(relationInfo.id2).size() - 1);
+				relatedToSpan = new Span(first.start, last.end);
+				relatedToType = Mapper.getEntityTypeId(entityTypes.get(relationInfo.id2));
+			}
+			else {
+				continue; // invalid value in role2
+			}
+			
+			// create a modifier for "Argument"
+			Modifier modifier = null;
+			if(spanToModifier.containsKey(argumentSpan)) {
+				// an entity with the same span has already been added to the cas
+				modifier = spanToModifier.get(argumentSpan);
+			} 
+			else {
+				// this entity needs to be addded to the cas
+				modifier = new Modifier(jCas, argumentSpan.start, argumentSpan.end);
+				modifier.setTypeID(argumentType);
+				modifier.setId(identifiedAnnotationId++);
+				modifier.setDiscoveryTechnique(CONST.NE_DISCOVERY_TECH_GOLD_ANNOTATION);
+				modifier.setConfidence(1);
+				modifier.addToIndexes();
+				spanToModifier.put(argumentSpan, modifier);
+			}
+			
+			// create an entity mention for "Related_to"
+			EntityMention entityMention = null;
+			if(spanToEntity.containsKey(relatedToSpan)) {
+				// an entity with the same span has already been added to the cas
+				entityMention = spanToEntity.get(relatedToSpan);
+			} 
+			else {
+				// this entity needs to be addded to the cas
+				entityMention = new EntityMention(jCas, relatedToSpan.start, relatedToSpan.end);
+				entityMention.setTypeID(relatedToType);
+				entityMention.setId(identifiedAnnotationId++);
+				entityMention.setDiscoveryTechnique(CONST.NE_DISCOVERY_TECH_GOLD_ANNOTATION);
+				entityMention.setConfidence(1);
+				entityMention.addToIndexes();
+				spanToEntity.put(relatedToSpan, entityMention);
+			}
+
+			RelationArgument relationArgument1 = new RelationArgument(jCas);
+			relationArgument1.setId(relationArgumentId++);
+			relationArgument1.setArgument(modifier);
+			relationArgument1.setRole(Constants.mipacqArgumentName);
+
+			RelationArgument relationArgument2 = new RelationArgument(jCas);
+			relationArgument2.setId(relationArgumentId++);
+			relationArgument2.setArgument(entityMention);
+			relationArgument2.setRole(Constants.mipacqRelatedToName);
+
+			BinaryTextRelation binaryTextRelation = new BinaryTextRelation(jCas);
+			binaryTextRelation.setArg1(relationArgument1);
+			binaryTextRelation.setArg2(relationArgument2);
+			binaryTextRelation.setId(relationId++);
+			binaryTextRelation.setCategory(relationInfo.category);
+			binaryTextRelation.setDiscoveryTechnique(CONST.REL_DISCOVERY_TECH_GOLD_ANNOTATION);
+			binaryTextRelation.setConfidence(1);
+			binaryTextRelation.addToIndexes();
+		}
+
+	}
+	
+	/**
+	 * Add to the CAS the instances of all relations of the form:
+	 * [Argument] [relation] [Related_to] (e.g. [skin] location_of [rash]
+	 * Create an EntityMention for both [Argument] and [Related_to].
+	 */
+	private void addTheRestOfRelations(
+			JCas jCas, 
+			HashMap<String, ArrayList<Span>> entityMentions,
+			HashMap<String, String> entityTypes,
+			ArrayList<RelationInfo> relationInfos,
+			HashMap<Span, EntityMention> spanToEntity) {
+		
+		// relations between unique pairs of entities in gold standard
+		HashSet<RelationInfo> uniqueRelations = new HashSet<RelationInfo>();
+		
 		// add relations and relation arguments to the cas
 		for(RelationInfo relationInfo : relationInfos) {
 
@@ -116,6 +271,7 @@ public class GoldEntityAndRelationReader extends JCasAnnotator_ImplBase {
 			}
 
 			// ignore all entities that ctakes cannot extract (and relations between them)
+			// e.g. all instances of degree_of relation will be discarded
 			if(Mapper.getEntityTypeId(entityTypes.get(relationInfo.id1)) == CONST.NE_TYPE_ID_UNKNOWN) {
 				continue;
 			}
@@ -190,6 +346,16 @@ public class GoldEntityAndRelationReader extends JCasAnnotator_ImplBase {
 			binaryTextRelation.setConfidence(1);
 			binaryTextRelation.addToIndexes();
 		}
+	}
+	
+	/**
+	 * Add the entities, that haven't been added to the CAS while adding relation instances. 
+	 */
+	private void addTheRestOfEntities(
+			JCas jCas, 
+			HashMap<String, ArrayList<Span>> entityMentions,
+			HashMap<String, String> entityTypes,
+			HashMap<Span, EntityMention> spanToEntity) {
 
 		// add the rest of entities to the cas
 		for(Map.Entry<String, ArrayList<Span>> entry : entityMentions.entrySet()) {
@@ -219,6 +385,10 @@ public class GoldEntityAndRelationReader extends JCasAnnotator_ImplBase {
 		}
 	}
 	
+	/**
+	 * Show relation instances that are currently present in the CAS. 
+	 * This can be useful for debugging.  
+	 */
 	private void showAddedRelations(JCas jCas, boolean showBothArguments) {
 		
 		for(BinaryTextRelation binaryTextRelation : org.uimafit.util.JCasUtil.select(jCas, BinaryTextRelation.class)) {
