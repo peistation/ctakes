f8234d6a2ac29dc608028d66bbf2d12b2b526cd1
dimdym <dimdym@unknown>
2012-04-23 17:14:49 +0000

    Added argument role names for Share project.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1386189 13f79535-47bb-0310-9956-ffa450edef68

--- relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/cr/Constants.java
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/cr/Constants.java
@@ -14,8 +14,10 @@ public class Constants {
 	// names of semantic arguments
 	public static final String mipacqArgumentName = "Argument";
 	public static final String mipacqRelatedToName = "Related_to";
-	public static final String sharpArgumentName = "Argument_CU";
-	public static final String sharpRelatedToName = "Related_to_CU";
+	public static final String sharpArgumentName = "Related_to_CU";
+	public static final String sharpRelatedToName = "Argument_CU";
+	public static final String shareArgumentName = "Related_to";
+	public static final String shareRelatedToName = "Argument";
 	
 	// high priority sharp relations 
 	public static final HashSet<String> relationSet = new HashSet<String>(Arrays.asList("location_of", "degree_of"));
