f7dc964a78a47efafb8576532ba57f585624d022
dimdym <dimdym@unknown>
2012-06-07 20:40:56 +0000

    added stats calculations for entity types
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1386361 13f79535-47bb-0310-9956-ffa450edef68

--- relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/data/Stats.java
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/data/Stats.java
@@ -6,11 +6,14 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.chboston.cnlp.ctakes.relationextractor.cr.Mapper;
 import org.chboston.cnlp.ctakes.relationextractor.knowtator.RelationInfo;
 import org.chboston.cnlp.ctakes.relationextractor.knowtator.XMLReader;
 import org.jdom.Document;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
+
+import edu.mayo.bmi.uima.core.type.constants.CONST;
 
 /**
  * Calculate relation frequencies in a data set that consists of XML files exported from Knowtator.
@@ -22,8 +25,49 @@ public class Stats {
 		
 	// read all the relations in the data or just the SHARP ones?
 	public static final boolean readOnlySharpRelations = true;
-	
-	public static void compute(String inputDir) throws JDOMException, IOException {
+
+	public static void getEntityStats(String inputDir) throws JDOMException, IOException {
+
+		File dir = new File(inputDir);
+
+		// key: entity type, value: total instances of this entity
+		HashMap<String, Integer> entityCounts = new HashMap<String, Integer>(); 
+		// total number of entities
+		int totalEntityCount = 0;
+
+		FilenameFilter filter = new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".xml");
+			}
+		};
+
+		for(String file : dir.list(filter)) {
+
+			SAXBuilder builder = new SAXBuilder();
+			Document document = builder.build(new File(inputDir, file));
+
+			for(String enitytType : XMLReader.getEntityTypes(document).values()) {
+
+				if(Mapper.getEntityTypeId(enitytType) == CONST.NE_TYPE_ID_UNKNOWN) {
+					continue;
+				}
+				
+				totalEntityCount++;
+
+				if(entityCounts.containsKey(enitytType)) {
+					entityCounts.put(enitytType, entityCounts.get(enitytType) + 1);
+				} 
+				else {
+					entityCounts.put(enitytType, 1);
+				}
+			}
+		}
+
+		System.out.println(inputDir);
+		reportStats(entityCounts, totalEntityCount);
+	}
+
+	public static void getRelationStats(String inputDir) throws JDOMException, IOException {
 		
 		File dir = new File(inputDir);
 		
@@ -63,20 +107,21 @@ public class Stats {
 		}
 
 		System.out.println(inputDir);
-		reportRelationStats(relationCounts, totalRelationCount);
+		reportStats(relationCounts, totalRelationCount);
 	}
 	
-  public static void reportRelationStats(HashMap<String, Integer> relationCounts, int totalRelationCount) {
+  public static void reportStats(HashMap<String, Integer> counts, int totalCount) {
 
-    for(String relation : relationCounts.keySet()) {
-    	System.out.format("%-25s %5d (%.2f%%)\n", relation, relationCounts.get(relation), (double) relationCounts.get(relation) * 100 / totalRelationCount);
+    for(String key : counts.keySet()) {
+    	System.out.format("%-25s %5d (%.2f%%)\n", key, counts.get(key), (double) counts.get(key) * 100 / totalCount);
     }
     
-    System.out.format("\n%-25s %5d (%d%%)\n\n", "total", totalRelationCount, 100);
+    System.out.format("\n%-25s %5d (%d%%)\n\n", "total", totalCount, 100);
   }
   
 	public static void main(String[] args) throws JDOMException, IOException {
 		
-		Stats.compute(Constants.sharpTrainXmlPath);
+		Stats.getEntityStats(Constants.sharpTrainXmlPath);
+		Stats.getRelationStats(Constants.sharpTrainXmlPath);
 	}
 }
