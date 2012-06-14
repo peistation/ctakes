ace02318d138ba7ce64bc505a775571ca3207228
Steven Bethard <stevenbethard@apache.org>
2012-06-14 20:38:46 +0000

    Add descriptor generation to the end of RelationExtractorTrain
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1386378 13f79535-47bb-0310-9956-ffa450edef68

--- relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/util/RelationExtractorTrain.java
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/util/RelationExtractorTrain.java
@@ -1,10 +1,16 @@
 package org.chboston.cnlp.ctakes.relationextractor.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.util.XMLInputSource;
+import org.apache.uima.util.XMLParser;
 import org.chboston.cnlp.ctakes.relationextractor.ae.DegreeOfRelationExtractorAnnotator;
 import org.chboston.cnlp.ctakes.relationextractor.ae.EntityMentionPairRelationExtractorAnnotator;
 import org.chboston.cnlp.ctakes.relationextractor.ae.RelationExtractorAnnotator;
@@ -12,8 +18,13 @@ import org.chboston.cnlp.ctakes.relationextractor.eval.MultiClassLIBSVMDataWrite
 import org.chboston.cnlp.ctakes.relationextractor.eval.RelationExtractorEvaluation;
 import org.chboston.cnlp.ctakes.relationextractor.eval.RelationExtractorEvaluation.ParameterSettings;
 import org.cleartk.classifier.DataWriterFactory;
+import org.cleartk.classifier.jar.GenericJarClassifierFactory;
 import org.cleartk.util.Options_ImplBase;
 import org.kohsuke.args4j.Option;
+import org.uimafit.factory.AggregateBuilder;
+import org.uimafit.factory.AnalysisEngineFactory;
+import org.uimafit.factory.ConfigurationParameterFactory;
+import org.xml.sax.SAXException;
 
 /**
  * This class is intended for training production models.
@@ -36,6 +47,12 @@ public class RelationExtractorTrain {
         usage = "if true runs the degree of relation extractor otherwise "
             + "it uses the normal entity mention pair relation extractor")
     public boolean runDegreeOf = false;
+
+    @Option(
+        name = "--desc-dir",
+        usage = "specify the directory to write out description files",
+        required = false)
+    public File descDir = new File("desc/analysis_engine");
   }
 	
 	public static void main(String[] args) throws Exception {
@@ -89,5 +106,43 @@ public class RelationExtractorTrain {
     evaluation.train(collectionReader, modelsDir);
 
     System.out.println("model written to " + modelsDir.getAbsolutePath());
-	}
+    
+    // create the description
+    AnalysisEngineDescription relationExtractorDescription = AnalysisEngineFactory.createPrimitiveDescription(
+        annotatorClass,
+        RelationExtractorAnnotator.PARAM_PROBABILITY_OF_KEEPING_A_NEGATIVE_EXAMPLE,
+        params.probabilityOfKeepingANegativeExample,
+        GenericJarClassifierFactory.PARAM_CLASSIFIER_JAR_PATH,
+        new File(modelsDir, "model.jar").getPath());
+    if (annotatorClass == EntityMentionPairRelationExtractorAnnotator.class) {
+      ConfigurationParameterFactory.addConfigurationParameters(
+          relationExtractorDescription,
+          EntityMentionPairRelationExtractorAnnotator.PARAM_CLASSIFY_BOTH_DIRECTIONS,
+          params.classifyBothDirections);
+    }
+    
+    // write out the XML version of the description
+    writeDesc(options.descDir, annotatorClass.getSimpleName(), relationExtractorDescription);
+    
+    // create the aggregate description
+    AggregateBuilder builder = new AggregateBuilder();
+    File preprocessDescFile = new File("desc/analysis_engine/RelationExtractorPreprocessor.xml");
+    XMLParser parser = UIMAFramework.getXMLParser();
+    XMLInputSource source = new XMLInputSource(preprocessDescFile);
+    builder.add(parser.parseAnalysisEngineDescription(source));
+    builder.add(relationExtractorDescription);
+    AnalysisEngineDescription aggregateDescription = builder.createAggregateDescription();
+    
+    // write out the XML version of the aggregate description
+    writeDesc(options.descDir, "RelationExtractorAggregate", aggregateDescription);
+  }
+
+  private static void writeDesc(File descDir, String filePrefix, AnalysisEngineDescription desc)
+      throws SAXException, IOException {
+    File descFile = new File(descDir, filePrefix + ".xml");
+    FileOutputStream output = new FileOutputStream(descFile);
+    desc.toXML(output);
+    output.close();
+    System.out.println("description written to " + descFile);
+  }
 }
