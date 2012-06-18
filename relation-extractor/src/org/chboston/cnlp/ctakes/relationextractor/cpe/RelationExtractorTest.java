acb5aa3a86a36fbcb5bc880518f605754a743686
dimdym <dimdym@unknown>
2012-06-18 20:57:35 +0000

    Pipeline for running relation extraction on notes in a directory
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1386384 13f79535-47bb-0310-9956-ffa450edef68

---
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/cpe/RelationExtractorTest.java
@@ -0,0 +1,52 @@
+package org.chboston.cnlp.ctakes.relationextractor.cpe;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.chboston.cnlp.ctakes.relationextractor.ae.EntityMentionPairRelationExtractorAnnotator;
+import org.chboston.cnlp.ctakes.relationextractor.cr.GoldEntityAndRelationReader;
+import org.uimafit.component.xwriter.XWriter;
+import org.uimafit.factory.AnalysisEngineFactory;
+import org.uimafit.factory.CollectionReaderFactory;
+import org.uimafit.factory.TypeSystemDescriptionFactory;
+import org.uimafit.pipeline.SimplePipeline;
+import edu.mayo.bmi.uima.core.cr.FilesInDirectoryCollectionReader;
+
+public class RelationExtractorTest {
+
+	public static void main(String[] args) throws UIMAException, IOException {
+		
+		TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory.createTypeSystemDescriptionFromPath(
+				"/home/dima/workspaces/ctakes/common-type-system/desc/common_type_system.xml");
+    
+		CollectionReader collectionReader = CollectionReaderFactory.createCollectionReaderFromPath(
+				"/home/dima/workspaces/ctakes/core/desc/collection_reader/FilesInDirectoryCollectionReader.xml",
+				"InputDirectory",
+				"/home/dima/sharp/cloud/mipacq/text/dev");
+		
+		AnalysisEngine relationExtractor = AnalysisEngineFactory.createAnalysisEngineFromPath(
+				"/home/dima/workspaces/ctakes/relation-extractor/desc/analysis_engine/RelationExtractorAggregate.xml",
+				EntityMentionPairRelationExtractorAnnotator.PARAM_CLASSIFY_BOTH_DIRECTIONS,
+				false,
+				EntityMentionPairRelationExtractorAnnotator.PARAM_PROBABILITY_OF_KEEPING_A_NEGATIVE_EXAMPLE,
+				0.5f,
+				EntityMentionPairRelationExtractorAnnotator.PARAM_PRINT_ERRORS,
+				false,
+				EntityMentionPairRelationExtractorAnnotator.PARAM_IS_TRAINING,
+				false);
+    
+    AnalysisEngine xWriter = AnalysisEngineFactory.createPrimitive(
+    		XWriter.class,
+    		typeSystemDescription,
+    		XWriter.PARAM_OUTPUT_DIRECTORY_NAME,
+    		"/home/dima/temp/");
+		
+		SimplePipeline.runPipeline(collectionReader, relationExtractor, xWriter);
+	}
+}
+
