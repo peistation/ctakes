616d7046b0221c726f50839a4d36b80c743a9343
dimdym <dimdym@unknown>
2012-06-18 17:29:09 +0000

    Switch from using CasConsumer to XWriter
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1386382 13f79535-47bb-0310-9956-ffa450edef68

--- relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/cpe/TestGoldEntityAndRelationReader.java
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/cpe/TestGoldEntityAndRelationReader.java
@@ -7,6 +7,7 @@ import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.chboston.cnlp.ctakes.relationextractor.cr.GoldEntityAndRelationReader;
+import org.uimafit.component.xwriter.XWriter;
 import org.uimafit.factory.AnalysisEngineFactory;
 import org.uimafit.factory.CollectionReaderFactory;
 import org.uimafit.factory.TypeSystemDescriptionFactory;
@@ -20,24 +21,24 @@ public class TestGoldEntityAndRelationReader {
 		TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory.createTypeSystemDescriptionFromPath(
 				"/home/dima/workspaces/ctakes/common-type-system/desc/common_type_system.xml");
 		
-		CollectionReaderDescription reader = CollectionReaderFactory.createDescription(
+		CollectionReaderDescription collectionReader = CollectionReaderFactory.createDescription(
 				FilesInDirectoryCollectionReader.class,
 				typeSystemDescription,
 				"InputDirectory",
 				"/home/dima/sharp/cloud/sharp/text/train/");
 		
-		AnalysisEngineDescription gold = AnalysisEngineFactory.createPrimitiveDescription(
+		AnalysisEngineDescription goldAnnotator = AnalysisEngineFactory.createPrimitiveDescription(
 				GoldEntityAndRelationReader.class,
 				typeSystemDescription,
 				"InputDirectory",
 				"/home/dima/sharp/cloud/sharp/xml/train/");
 
-		AnalysisEngineDescription consumer = AnalysisEngineFactory.createPrimitiveDescription(
-				CasConsumer.class,
-				typeSystemDescription);
-		
-		SimplePipeline.runPipeline(reader, gold, consumer);
-		
-		
+    AnalysisEngineDescription xWriter = AnalysisEngineFactory.createPrimitiveDescription(
+        XWriter.class,
+        typeSystemDescription,
+        XWriter.PARAM_OUTPUT_DIRECTORY_NAME,
+        "/home/dima/temp/");
+    
+		SimplePipeline.runPipeline(collectionReader, goldAnnotator, xWriter);
 	}
 }
