b09386bd3e98ea82b398e71ca01ac15e1ae3338b
dimdym <dimdym@unknown>
2012-06-19 14:17:37 +0000

    Changed the path from absolute to relative. Used command line arguments instead of hardcoded input/output paths.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1386387 13f79535-47bb-0310-9956-ffa450edef68

--- relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/pipelines/RelationExtractorTest.java
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/pipelines/RelationExtractorTest.java
@@ -7,6 +7,8 @@ import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.chboston.cnlp.ctakes.relationextractor.ae.EntityMentionPairRelationExtractorAnnotator;
+import org.cleartk.util.Options_ImplBase;
+import org.kohsuke.args4j.Option;
 import org.uimafit.component.xwriter.XWriter;
 import org.uimafit.factory.AnalysisEngineFactory;
 import org.uimafit.factory.CollectionReaderFactory;
@@ -14,24 +16,48 @@ import org.uimafit.factory.TypeSystemDescriptionFactory;
 import org.uimafit.pipeline.SimplePipeline;
 import edu.mayo.bmi.uima.core.cr.FilesInDirectoryCollectionReader;
 
+/**
+ * A simple pipeline that first runs all the components that are necessary for the relation extraction
+ * module. Next, it runs the relation extraction annotator. Finally, it saves the annotations as xmi files.
+ * 
+ * @author dmitriy dligach
+ *
+ */
 public class RelationExtractorTest {
 
+  public static class Options extends Options_ImplBase {
+
+    @Option(
+        name = "--input-dir",
+        usage = "specify the path to the directory containing the clinical notes to be processed",
+        required = true)
+    public String inputDirectory;
+    
+    @Option(
+        name = "--output-dir",
+        usage = "specify the path to the directory where the output xmi files are to be saved",
+        required = true)
+    public String outputDirectory;
+  }
+  
 	public static void main(String[] args) throws UIMAException, IOException {
 		
+		Options options = new Options();
+		options.parseOptions(args);
+
 		TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory.createTypeSystemDescriptionFromPath(
 				"../common-type-system/desc/common_type_system.xml");
     
 		CollectionReader collectionReader = CollectionReaderFactory.createCollectionReaderFromPath(
 				"../core/desc/collection_reader/FilesInDirectoryCollectionReader.xml",
 				FilesInDirectoryCollectionReader.PARAM_INPUTDIR,
-				"/home/dima/sharp/cloud/mipacq/text/dev");
-		
+				options.inputDirectory);
+
+		// the parameters must match the training parameters
 		AnalysisEngine relationExtractor = AnalysisEngineFactory.createAnalysisEngineFromPath(
-				"/home/dima/workspaces/ctakes/relation-extractor/desc/analysis_engine/RelationExtractorAggregate.xml",
+				"desc/analysis_engine/RelationExtractorAggregate.xml",
 				EntityMentionPairRelationExtractorAnnotator.PARAM_CLASSIFY_BOTH_DIRECTIONS,
-				false,
-				EntityMentionPairRelationExtractorAnnotator.PARAM_PROBABILITY_OF_KEEPING_A_NEGATIVE_EXAMPLE,
-				0.5f,
+				true,
 				EntityMentionPairRelationExtractorAnnotator.PARAM_PRINT_ERRORS,
 				false,
 				EntityMentionPairRelationExtractorAnnotator.PARAM_IS_TRAINING,
@@ -41,9 +67,8 @@ public class RelationExtractorTest {
     		XWriter.class,
     		typeSystemDescription,
     		XWriter.PARAM_OUTPUT_DIRECTORY_NAME,
-    		"/home/dima/temp/");
+    		options.outputDirectory);
 		
 		SimplePipeline.runPipeline(collectionReader, relationExtractor, xWriter);
 	}
 }
-
