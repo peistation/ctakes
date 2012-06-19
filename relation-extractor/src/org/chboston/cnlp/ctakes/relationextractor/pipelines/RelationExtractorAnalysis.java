4ed6277d7d6cdff9850d5bfe8f2f5fb2a681ddec
dimdym <dimdym@unknown>
2012-06-19 16:05:46 +0000

    Added a sample consumer of relation annotations.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/branches/SHARPn-cTAKES@1386388 13f79535-47bb-0310-9956-ffa450edef68

---
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/pipelines/RelationExtractorAnalysis.java
@@ -0,0 +1,75 @@
+package org.chboston.cnlp.ctakes.relationextractor.pipelines;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.chboston.cnlp.ctakes.relationextractor.ae.EntityMentionPairRelationExtractorAnnotator;
+import org.chboston.cnlp.ctakes.relationextractor.eval.XMIReader;
+import org.cleartk.util.Options_ImplBase;
+import org.kohsuke.args4j.Option;
+import org.uimafit.component.xwriter.XWriter;
+import org.uimafit.factory.AnalysisEngineFactory;
+import org.uimafit.factory.CollectionReaderFactory;
+import org.uimafit.factory.TypeSystemDescriptionFactory;
+import org.uimafit.pipeline.SimplePipeline;
+import edu.mayo.bmi.uima.core.cr.FilesInDirectoryCollectionReader;
+
+/**
+ * 
+ * Analyze the annotation obtained by running the relation extractor on clinical notes.
+ * The results of running the relation extractor are expected to be saved in xmi files.
+ * 
+ * @author dmitriy dligach
+ *
+ */
+public class RelationExtractorAnalysis {
+
+  public static class Options extends Options_ImplBase {
+
+    @Option(
+        name = "--input-dir",
+        usage = "specify the path to the directory containing the clinical notes to be processed",
+        required = true)
+    public File inputDirectory;
+  }
+  
+	public static void main(String[] args) throws Exception {
+		
+		Options options = new Options();
+		options.parseOptions(args);
+
+		TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory.createTypeSystemDescriptionFromPath(
+				"../common-type-system/desc/common_type_system.xml");
+		
+		List<File> trainFiles = Arrays.asList(options.inputDirectory.listFiles());
+    CollectionReader collectionReader = getCollectionReader(trainFiles, typeSystemDescription);
+		
+    AnalysisEngine relationExtractorConsumer = AnalysisEngineFactory.createPrimitive(
+    		RelationExtractorConsumer.class);
+    		
+		SimplePipeline.runPipeline(collectionReader, relationExtractorConsumer);
+	}
+	
+  private static CollectionReader getCollectionReader(List<File> items, 
+  		TypeSystemDescription typeSystemDescription) throws Exception {
+
+  	// convert the List<File> to a String[]
+    String[] paths = new String[items.size()];
+    for (int i = 0; i < paths.length; ++i) {
+      paths[i] = items.get(i).getPath();
+    }
+    
+    // return a reader that will load each of the XMI files
+    return CollectionReaderFactory.createCollectionReader(
+        XMIReader.class,
+        typeSystemDescription,
+        XMIReader.PARAM_FILES,
+        paths);
+  }
+}
---
+++ relation-extractor/src/org/chboston/cnlp/ctakes/relationextractor/pipelines/RelationExtractorConsumer.java
@@ -0,0 +1,48 @@
+package org.chboston.cnlp.ctakes.relationextractor.pipelines;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.jcas.JCas;
+import org.uimafit.component.JCasAnnotator_ImplBase;
+import org.uimafit.util.JCasUtil;
+
+import edu.mayo.bmi.uima.core.type.relation.BinaryTextRelation;
+import edu.mayo.bmi.uima.core.type.textsem.IdentifiedAnnotation;
+
+/**
+ * This is a sample relation annotation consumer. 
+ * Currently it displays the relation annotations.
+ * 
+ * @author dmitriy dligach
+ *
+ */
+public class RelationExtractorConsumer extends JCasAnnotator_ImplBase {
+
+	@Override
+  public void process(JCas jCas) throws AnalysisEngineProcessException {
+
+    JCas systemView;
+    try {
+      systemView = jCas.getView(CAS.NAME_DEFAULT_SOFA);
+    } catch (CASException e) {
+      throw new AnalysisEngineProcessException(e);
+    }	  
+    
+    for(BinaryTextRelation binaryTextRelation : JCasUtil.select(systemView, BinaryTextRelation.class)) {
+    	String category = binaryTextRelation.getCategory();
+    	
+    	IdentifiedAnnotation entity1 = (IdentifiedAnnotation) binaryTextRelation.getArg1().getArgument();
+    	IdentifiedAnnotation entity2 = (IdentifiedAnnotation) binaryTextRelation.getArg2().getArgument();
+    	
+    	String arg1 = entity1.getCoveredText();
+    	String arg2 = entity2.getCoveredText();
+    	
+    	int type1 = entity1.getTypeID();
+    	int type2 = entity2.getTypeID();
+    	
+    	System.out.format("%s(%s/%d, %s/%d)\n", category, arg1, type1, arg2, type2);
+    }
+  }
+
+}
