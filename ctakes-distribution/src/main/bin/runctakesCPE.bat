e38b9a8d9d7a69c5c9e625c6d5f0dc2fd71422b8
Pei Chen <chenpei@apache.org>
2012-10-19 20:49:28 +0000

    Added default log4j configuration.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/trunk@1400281 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-distribution/src/main/bin/runctakesCPE.bat
+++ ctakes-distribution/src/main/bin/runctakesCPE.bat
@@ -35,6 +35,6 @@ goto end
 
 :okHome
 cd %CTAKES_HOME%
-java -cp "%CTAKES_HOME%/lib/*;%CTAKES_HOME%/desc/*" -Djava.util.logging.config.file=%CTAKES_HOME%/config/Logger.properties -Xms512M -Xmx1024M org.apache.uima.tools.cpm.CpmFrame
+java -cp "%CTAKES_HOME%/lib/*;%CTAKES_HOME%/desc/*" -Dlog4j.configuration=file:/%CTAKES_HOME%/config/log4j.xml -Xms512M -Xmx1024M org.apache.uima.tools.cpm.CpmFrame
 
 :end
--- ctakes-distribution/src/main/bin/runctakesCVD.sh
+++ ctakes-distribution/src/main/bin/runctakesCVD.sh
@@ -33,4 +33,4 @@ PRGDIR=`dirname "$PRG"`
 [ -z "$CTAKES_HOME" ] && CTAKES_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
 
 cd $CTAKES_HOME
-java -cp $CTAKES_HOME/lib/*:$CTAKES_HOME/desc/ -Djava.util.logging.config.file=%CTAKES_HOME%/config/Logger.properties -Xms512M -Xmx1024M org.apache.uima.tools.cvd.CVD
+java -cp $CTAKES_HOME/lib/*:$CTAKES_HOME/desc/ -Dlog4j.configuration=file:/%CTAKES_HOME%/config/log4j.xml -Xms512M -Xmx1024M org.apache.uima.tools.cvd.CVD
