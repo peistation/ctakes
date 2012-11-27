5e6786e9f1ec84a357a386d02a7da2d636fa9aa1
Chen Lin <clin@apache.org>
2012-11-27 16:17:46 +0000

    test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/trunk@1414245 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ctakes-temporal/src/main/java/org/apache/ctakes/temporal/ae/feature/selection/FeatureSelectionExtractor.java
@@ -0,0 +1,35 @@
+package org.apache.ctakes.temporal.ae.feature.selection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cleartk.classifier.feature.transform.TrainableExtractor_ImplBase;
+import org.cleartk.classifier.feature.transform.TransformableFeature;
+import org.cleartk.classifier.Feature;
+import org.cleartk.classifier.Instance;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+
+public abstract class FeatureSelectionExtractor<OUTCOME_T> extends
+		TrainableExtractor_ImplBase<OUTCOME_T> implements Predicate<Feature> {
+			
+		public FeatureSelectionExtractor(String name) {
+		    super(name);
+		}
+
+		@Override
+		public Instance<OUTCOME_T> transform(Instance<OUTCOME_T> instance) {
+		    List<Feature> features = new ArrayList<Feature>();
+		    for (Feature feature : instance.getFeatures()) {
+		    	if (this.isTransformable(feature)) {
+			        // Filter down to selected features
+			        features.addAll(Collections2.filter(((TransformableFeature) feature).getFeatures(), this));
+			    } else {
+			        // Pass non-relevant features through w/o filtering
+			        features.add(feature);
+			    }
+			}
+			return new Instance<OUTCOME_T>(instance.getOutcome(), features);
+		}
+}
