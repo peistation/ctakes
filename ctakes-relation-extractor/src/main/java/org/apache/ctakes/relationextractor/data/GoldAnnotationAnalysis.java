661b8189c4dde876de0e41f2dbf34480a9560fac
Dmitriy Dligach <dligach@apache.org>
2013-01-07 15:53:31 +0000

    added init version of two classes for analyzing gold standard annotations
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/trunk@1429860 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ctakes-relation-extractor/src/main/java/org/apache/ctakes/relationextractor/data/GoldAnnotationAnalysis.java
@@ -0,0 +1,80 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.ctakes.relationextractor.data;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.ctakes.relationextractor.eval.XMIReader;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.collection.CollectionReader;
+import org.cleartk.util.Options_ImplBase;
+import org.kohsuke.args4j.Option;
+import org.uimafit.factory.AnalysisEngineFactory;
+import org.uimafit.factory.CollectionReaderFactory;
+import org.uimafit.pipeline.SimplePipeline;
+
+/**
+ * 
+ * Pipeline for analyzing gold annotations.
+ *  
+ * @author dmitriy dligach
+ *
+ */
+public class GoldAnnotationAnalysis {
+
+  public static class Options extends Options_ImplBase {
+
+    @Option(
+        name = "--input-dir",
+        usage = "specify the path to the directory containing the clinical notes to be processed",
+        required = true)
+    public File inputDirectory;
+  }
+  
+	public static void main(String[] args) throws Exception {
+		
+		Options options = new Options();
+		options.parseOptions(args);
+
+		List<File> trainFiles = Arrays.asList(options.inputDirectory.listFiles());
+    CollectionReader collectionReader = getCollectionReader(trainFiles);
+		
+    AnalysisEngine relationExtractorConsumer = AnalysisEngineFactory.createPrimitive(
+    		GoldAnnotationStats.class);
+    		
+		SimplePipeline.runPipeline(collectionReader, relationExtractorConsumer);
+	}
+	
+  private static CollectionReader getCollectionReader(List<File> items) throws Exception {
+
+  	// convert the List<File> to a String[]
+    String[] paths = new String[items.size()];
+    for (int i = 0; i < paths.length; ++i) {
+      paths[i] = items.get(i).getPath();
+    }
+    
+    // return a reader that will load each of the XMI files
+    return CollectionReaderFactory.createCollectionReader(
+        XMIReader.class,
+        XMIReader.PARAM_FILES,
+        paths);
+  }
+}
