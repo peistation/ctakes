aead564bea683f5567291076c8e78a0d46670f37
Tim Miller <tmill@apache.org>
2013-02-13 12:31:40 +0000

    Fixed small bug in utility class that only I use.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ctakes/trunk@1445570 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-utils/src/main/java/org/apache/ctakes/utils/struct/CounterTreeMap.java
+++ ctakes-utils/src/main/java/org/apache/ctakes/utils/struct/CounterTreeMap.java
@@ -7,13 +7,13 @@ import java.util.TreeMap;
 // This class is a simplifying class which makes it easy to build hashes to keep track of counts
 // and write less boilerplate code.  If you just call it with an object, it will increment the
 // object's count by 1, initializing it to zero first if necessary.
-public class CounterTreeMap<K> implements Comparator<K>{
+public class CounterTreeMap<K> {
 
 //	IntValueComparator<K> comp = new IntValueComparator<K>();
 	TreeMap<K,Integer> map = null;
 	
 	public CounterTreeMap(){
-		map = new TreeMap<K,Integer>(this);
+		map = new TreeMap<K,Integer>();
 //		super(this);
 	}
 	
@@ -33,11 +33,14 @@ public class CounterTreeMap<K> implements Comparator<K>{
 		map.put(key, map.get(key)+i);
 	}
 
-
-	@Override
-	public int compare(K o1, K o2) {
-		return map.get(o1) - map.get(o2);
+	public int size(){
+		return map.size();
 	}
+	
+//	@Override
+//	public int compare(K o1, K o2) {
+//		return o1 - o2;
+//	}
 
 	public Set<K> keySet() {
 		return map.keySet();
