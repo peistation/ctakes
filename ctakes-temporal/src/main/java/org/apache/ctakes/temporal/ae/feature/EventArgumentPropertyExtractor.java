4f592b871c26fd3fabb84d7328f8edadbd1afdea
Tim Miller <tmill@apache.org>
2013-06-05 21:35:57 +0000

    Completed method for event argument extraction.
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1490038 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-temporal/src/main/java/org/apache/ctakes/temporal/ae/feature/EventArgumentPropertyExtractor.java
+++ ctakes-temporal/src/main/java/org/apache/ctakes/temporal/ae/feature/EventArgumentPropertyExtractor.java
@@ -7,9 +7,11 @@ import java.util.List;
 import org.apache.ctakes.relationextractor.ae.features.RelationFeaturesExtractor;
 import org.apache.ctakes.typesystem.type.textsem.EventMention;
 import org.apache.ctakes.typesystem.type.textsem.IdentifiedAnnotation;
+import org.apache.ctakes.typesystem.type.textspan.Sentence;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.cleartk.classifier.Feature;
+import org.uimafit.util.JCasUtil;
 
 public class EventArgumentPropertyExtractor implements
     RelationFeaturesExtractor {
@@ -18,6 +20,24 @@ public class EventArgumentPropertyExtractor implements
   public List<Feature> extract(JCas jCas, IdentifiedAnnotation arg1,
       IdentifiedAnnotation arg2) throws AnalysisEngineProcessException {
     List<Feature> feats = new ArrayList<Feature>();
+    
+    Sentence coveringSent = JCasUtil.selectCovering(jCas, Sentence.class, arg1.getBegin(), arg1.getEnd()).get(0);
+    List<EventMention> events = JCasUtil.selectCovered(EventMention.class, coveringSent);
+    List<EventMention> realEvents = new ArrayList<EventMention>();
+    for(EventMention event : events){
+    	// filter out ctakes events
+    	if(event.getClass().equals(EventMention.class)){
+    		realEvents.add(event);
+    	}
+    }
+    events = realEvents;
+    EventMention anchor = events.get(0);
+    
+    if(arg1.getBegin() == anchor.getBegin() && arg1.getEnd() == anchor.getEnd()){
+    	feats.add(new Feature("Arg1LeftmostEvent"));
+    }else if(arg2.getBegin() == anchor.getBegin() && arg2.getEnd() == anchor.getEnd()){
+    	feats.add(new Feature("Arg2LeftmostEvent"));
+    }
     
     if(arg1 instanceof EventMention){
       feats.addAll(getEventFeats("mention1property", (EventMention)arg1));
