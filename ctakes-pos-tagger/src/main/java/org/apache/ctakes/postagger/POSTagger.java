e4ef804dc5d5e8f3d399fe43ebf9421c3395d912
Pei Chen <chenpei@apache.org>
2013-07-08 22:07:43 +0000

    CTAKES-97 - Since OpenNLP 1.5+, there is no need to load up the tag dictionary anymore. Removing unncessary/unused bit of code.
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1500985 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-pos-tagger/src/main/java/org/apache/ctakes/postagger/POSTagger.java
+++ ctakes-pos-tagger/src/main/java/org/apache/ctakes/postagger/POSTagger.java
@@ -89,41 +89,6 @@ public class POSTagger extends JCasAnnotator_ImplBase {
 	 */
 	public static final String POS_MODEL_FILE_PARAM = "PosModelFile";
 
-	/**
-	 * "TagDictionary" is an optional, single, string parameter that contains
-	 * the file name of the part-of-speech tag dictionary. For relevant
-	 * discussion of the difference between a "tag dictionary" and a
-	 * "dictionary" see:
-	 * <p><ul>
-	 * <li>
-	 * <a href="https://sourceforge.net/forum/forum.php?thread_id=1720863&forum_id=9943">PosTagger - with/without dictionaries? and ..</a>
-	 * <li> 
-	 * </li>
-	 * <a href="https://sourceforge.net/forum/forum.php?thread_id=1894043&forum_id=9943">Create a new dict file</a>
-	 * </li>
-	 * </ul>
-	 * For information about how to create a TagDictionary, please see the
-	 * README in top-level directory of this project.
-	 * 
-	 * @see TagDictionary
-	 * @see POSDictionary
-	 */
-	public static final String TAG_DICTIONARY_PARAM = "TagDictionary";
-
-	/**
-	 * "CaseSensitive" is a required, single, boolean parameter that specifies
-	 * how to access entries in the tag dictionary. If you give the value
-	 * "false", then you should really have a tag dictionary that is also case
-	 * insensitive. Please see the README in top-level directory of this project
-	 * for details on how to create a case insensitive tag dictionary.
-	 * 
-	 * <br>
-	 * This parameter has no effect if no tag dictionary is provided but is
-	 * required if a tag dictionary is provided.
-	 * 
-	 * @see POSDictionary#POSDictionary(String, boolean)
-	 * 
-	 */
 	public static final String CASE_SENSITIVE_PARAM = "CaseSensitive";
 
 	private opennlp.tools.postag.POSTaggerME tagger;
@@ -138,20 +103,6 @@ public class POSTagger extends JCasAnnotator_ImplBase {
 			File posModelFile = FileLocator.locateFile(posModelPath);
 			String modelFileAbsPath = posModelFile.getAbsolutePath();
 			logger.info("POS tagger model file: " + modelFileAbsPath);
-
-			boolean caseSensitive = (Boolean) uimaContext.getConfigParameterValue(CASE_SENSITIVE_PARAM);
-			String tagDictionaryPath = (String) uimaContext.getConfigParameterValue(TAG_DICTIONARY_PARAM);
-
-			TagDictionary tagDictionary = null;
-			if (tagDictionaryPath != null && !tagDictionaryPath.trim().equals("")) {
-				File tagDictFile = FileLocator.locateFile(tagDictionaryPath);
-				String tagDictFileAbsPath = tagDictFile.getAbsolutePath();
-				logger.info("POS tagger tag-dictionary: " + tagDictFileAbsPath);
-
-				tagDictionary = new POSDictionary(tagDictFileAbsPath, caseSensitive);
-			} else {
-				logger.info("No POS tagger tag-dictionary.");
-			}
 
 			FileInputStream fis = new FileInputStream(posModelFile);
 			POSModel modelFile = new POSModel(fis); // skip using the tag dictionary for now since OpenNLP (1.5) changed
